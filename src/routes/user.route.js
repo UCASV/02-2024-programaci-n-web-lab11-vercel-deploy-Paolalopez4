91f56ef08c0e37e9530365e0c3bb930ad6a13bf7
Paolalopez4 <00167523@uca.edu.sv>
2024-11-21 13:48:28 -0600

    Added template files

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,12 @@
-const { addUserController, showForm, allUsersController} = require('../controllers/user.controller');
-const router = require('express').Router();
+const {
+  addUserController,
+  showForm,
+  allUsersController,
+} = require("../controllers/user.controller");
+const router = require("express").Router();
 
-router.get('/', showForm);
-router.post('/home', addUserController)
-router.get('/allUsers', allUsersController)
+router.get("/", showForm);
+router.post("/home", addUserController);
+router.get("/allUsers", allUsersController);
 
 module.exports = router;
--- src/services/user.servie.js
+++ src/services/user.servie.js
@@ -1,16 +1,15 @@
-const {save, findAll, User} = require('../models/user.model');
+const { save, findAll, User } = require("../models/user.model");
 
-const addNewUser = (name, lastname, email)=>{
-    const newUser = new User(name, lastname, email);
-    return save(newUser);
-}
+const addNewUser = (name, lastname, email) => {
+  const newUser = new User(name, lastname, email);
+  return save(newUser);
+};
 
-const getAllUsers = ()=>{
-    return findAll();
-}
-
+const getAllUsers = () => {
+  return findAll();
+};
 
 module.exports = {
-    addNewUser,
-    getAllUsers,
-}
+  addNewUser,
+  getAllUsers,
+};
